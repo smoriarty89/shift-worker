affd5251470ffaa92603f6cbd1d9303716a5719d
smoriarty89 <shailene.moriarty@hotmail.com>
2026-09-18 00:33:26 +1000

    Update members.js

--- api/members.js
+++ api/members.js
@@ -14,7 +14,7 @@ async function kvGet(key) {
   return parsed;
 }
 
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
   try {
     if (req.method !== 'GET') return res.status(405).json({ error: 'Method not allowed' });
     const { code } = req.query;
